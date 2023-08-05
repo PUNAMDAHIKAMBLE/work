68463ff6b858a542e319e7868e279b3519823ade
PUNAMDAHIKAMBLE <141495822+PUNAMDAHIKAMBLE@users.noreply.github.com>
2023-08-05 17:31:16 +0530

    Add files via upload

---
+++ pd.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def sub():
+    a=int(input("Enter first no:"))
+    b=int(input("Enter second no"))
+    c=a+b
+    print(c)
+sub()
+
+
+# In[ ]:
+
+
+
+
